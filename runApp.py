0e490c043084508f63a7f11bde1466a1a33fa25c
Mitchell Tillman <mtillman14@gmail.com>
2023-05-22 12:34:30 -0400

    Standardizing some input vars

--- runApp.py
+++ runApp.py
@@ -6,14 +6,14 @@ from PySide6 import QtWidgets
 from loginWindow import Ui_loginWindow
 from setLoginWindowSignals import setLoginWindowSignals
 from initLoginSettings import initLoginInfo
-from initAllSettings import getSettingsPath
+# from initAllSettings import getSettingsPath
 
 # Initialize settings files needed by the application.
 # Currently uses the default settings path that most apps use.
 # For cheap/easy cloud integration in the future, can use Google Drive File Stream and set the settings path to the Google Drive folder.
 initLoginInfo()
 
-## Create application
+## Create application using QtDesigner file
 app = QtWidgets.QApplication(sys.argv)
 loginWindow = QtWidgets.QWidget()
 ui = Ui_loginWindow() # Instantiate (create an instance of) the Ui_loginWindow class
@@ -23,7 +23,7 @@ ui.setupUi(loginWindow)
 loginWindow.setFixedSize(loginWindow.size()) # Because QtDesigner seems unable to do this in the GUI
 ui.passwordEdit.setEchoMode(QtWidgets.QLineEdit.Password) # Because QtDesigner seems unable to do this in the GUI
 
-## Signals and Slots
+# Signals and Slots (can potentially be done in QtDesigner, don't yet know how)
 setLoginWindowSignals(ui)
 
 # Show window
