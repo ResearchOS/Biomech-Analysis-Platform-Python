37c337a1287796d257698978a9f6dd7ce177672d
Mitchell Tillman <mtillman14@gmail.com>
2023-10-27 11:50:48 -0500

    Can now load the digraph with attributes!

--- cli.py
+++ cli.py
@@ -5,24 +5,38 @@ import sys
 app = typer.Typer()
 
 import networkx as nx
-from networkx.drawing.nx_agraph import graphviz_layout, to_agraph
-from graphviz import Digraph
+from NavGraph import NavGraph
+from load_digraph import load_digraph
 
-@app.command(name="name_later")
-def name_later():
+@app.command(name="build")
+def build(fcn: callable, db_file: str) -> NavGraph:   
+    graph = NavGraph(fcn, db_file)
+    return graph
+
+@app.command(name="sel")
+def select_node(graph: NavGraph, input: str) -> None:
+    # Select the specified node (can be predecessor or successor)
+    uuid = input.to_number
+    graph.select_node(uuid) 
+
+@app.command(name="fav")
+def show_favorites(graph: NavGraph) -> None:
+    # TODO: Load the favorite nodes for this digraph.
     pass
 
-def build():    
-    graph = nx.DiGraph()
+@app.command(name="fav-set")
+def fav_set():
+    # Set a new favorite node
+    pass
 
-
-def draw_graph(graph: nx.DiGraph):
-    A = to_agraph(graph)
-    A.layout('dot')
-    A.draw('foo.png')
-
-    
+@app.command(name="fav-rem")
+def fav_rem():
+    # Remove a favorite node from the list.
+    pass
 
 
 if __name__ == "__main__":
-    app()
+    # app()
+    db_file = "/Users/mitchelltillman/Desktop/Work/MATLAB_Code/GitRepos/Biomech-Analysis-Platform-MATLAB/Databases/biomechOS.db"    
+    graph = NavGraph(load_digraph, db_file)
+    print(graph.nodes)
