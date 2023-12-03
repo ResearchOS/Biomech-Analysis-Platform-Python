a35f65a140d681acca3364506784945bef811c17
Mitchell Tillman <mtillman14@gmail.com>
2023-12-03 16:58:30 -0500

    I think Action.py should work now? I implemented all of the methods I think I'll need, need to figure out how to test it though.

--- action.py
+++ action.py
@@ -15,18 +15,26 @@ def get_current_user_object_id() -> str:
         raise AssertionError("There are multiple current users.")
     return result[0]
 
+def set_current_user_object_id(user_object_id: str) -> None:
+    """Set the ID of the current user."""
+    cursor = Action.conn.cursor()
+    sqlquery = f"INSERT INTO current_user (current_user_object_id) VALUES ('{user_object_id}')"
+    cursor.execute(sqlquery)
+    Action.conn.commit()
+
 class Action():    
 
     # Create a connection to the SQL database.  
     conn = sqlite3.connect("./SQL/database.db")
     
-    def __init__(self, name: str = None, closeable: bool = True, id: str = None):   
+    def __init__(self, name: str = None, closeable: bool = True, id: str = None, redo_of: str = None, user_object_id: str = None, timestamp_opened: datetime.datetime = None, timestamp_closed: datetime.datetime = None):   
         if not id:
+            # Creating a new action.
             id = str(uuid.uuid4()) # Making a new action.
-            timestamp_opened = datetime.datetime.utcnow()
-            timestamp_closed = None
-            redo_of = None
-            user_object_id = get_current_user_object_id()
+            if not timestamp_opened:
+                timestamp_opened = datetime.datetime.utcnow()
+            if not user_object_id:
+                user_object_id = get_current_user_object_id()
         else:
             # Loading an existing action.
             sqlquery = f"SELECT * FROM actions WHERE action_id = '{id}'"
@@ -46,10 +54,43 @@ class Action():
         self.closeable = closeable
         self.user_object_id = user_object_id
 
-    def execute(self):
-        """Execute the action, causing the current state of the referenced widgets and research objects to be the state in this Action."""
+    def redo(self) -> None:
+        """Execute the action, causing the current state of the referenced widgets and research objects to be the state in this Action."""        
+        # Create a new action, where "redo_of" is set to self.id.
+        action = Action(name = self.name, closeable = True, redo_of = self.id)
+        action.log() # Opens the action.
+        cursor = Action.conn.cursor()
+        table_names = ["data_values", "research_object_attributes", "data_address_schemas", "data_addresses"]
+        column_labels_list = [["action_id", "address_id", "schema_id", "VR_id", "PR_id", "scalar_value"], 
+                         ["action_id", "object_id", "attr_id", "attr_value", "child_of"],
+                         ["schema_id", "level_name", "level_index", "action_id", "dataset_id"],
+                         ["address_id", "schema_id", "level0", "level1", "level2", "level3", "level4", "level5", "level6", "level7", "level8", "level9", "action_id"]]
+        for idx, table_name in enumerate(table_names):
+            labels = column_labels_list[idx]
+            rows = Action._get_rows_of_action(action_id = self.id, table_name = table_name)
+            labels_formatted = ", ".join(labels)
+            action_id_index = labels.index("action_id")
+            for row in rows:                
+                row[action_id_index] = action.id # Update the action_id to the new action.
+                row_formatted = ", ".join(row)
+                sqlquery = f"INSERT INTO {table_name} ({labels_formatted}) VALUES ({row_formatted})"
+                cursor.execute(sqlquery, row)
+        
+        Action.conn.commit()
+        action.close()
+
+
+    def undo(self):
+        """Undo the action, causing the current state of the referenced widgets and research objects to be the state of the previous Action."""
         # Log the action to the database.
-        pass
+        prev_action = Action.get_most_recent(id = self.id)
+        prev_action.redo()
+
+    def _get_rows_of_action(action_id: str, table_name: str) -> list:
+        """Get the rows of a table that were created by an action."""
+        cursor = Action.conn.cursor()
+        sqlquery = f"SELECT * FROM {table_name} WHERE action_id = '{action_id}'"
+        return cursor.execute(sqlquery).fetchall()
 
     @abstractmethod
     def load(id: str = None):
@@ -61,21 +102,25 @@ class Action():
         if len(result) == 0:
             raise ValueError("No action with that ID exists.")
         id = result[0]
-        name = result[1] 
-                        
-        action = Action(name = name, id = id)                
-        return action
+        user_object_id = result[1]
+        name =  result[2]
+        timestamp_opened = result[3]
+        timestamp_closed = result[4]
+        redo_of = result[5]
+                    
+        return Action(name = name, id = id, user_object_id = user_object_id, timestamp_opened = timestamp_opened, timestamp_closed = timestamp_closed, redo_of = redo_of)
 
     @abstractmethod
-    def get_most_recent(id: str = None, time: str = None):
+    def get_most_recent(id: str = None, current_time: datetime.datetime = None) -> "Action":
         """Get the ID and all contents of the most recently performed action."""
-        if not id and not time:
-            return Action.get_current()
-        current_time = datetime.datetime.utcnow()
         cursor = Action.conn.cursor()
-        sqlquery = f"SELECT action_id FROM actions WHERE timestamp <= '{str(current_time)}' ORDER BY timestamp DESC LIMIT 1"
+        if not current_time:
+            current_time = datetime.datetime.utcnow()        
+        if id:
+            action = Action.load(id = id)
+            current_time = action.timestamp_opened
+        sqlquery = f"SELECT action_id FROM actions WHERE timestamp_opened <= '{str(current_time)}' ORDER BY timestamp DESC LIMIT 1"
         result = cursor.execute(sqlquery).fetchone()
-        result = []
         if result is None:
             return None
         id = result[0]
@@ -95,23 +140,34 @@ class Action():
         return True
 
     @abstractmethod
-    def open(name: str) -> "Action":
+    def open(name: str, closeable: bool = False) -> "Action":
+        """Creates a new action and logs it to the database, opening it for use. All operations performed after this will be logged to this action until it is closed."""
         if Action.is_open():
-            return Action.get_open(closeable = False)
+            return Action.get_open(closeable = closeable)
         
-        action = Action(name = name, closeable = True)
-        Action.conn.cursor().execute("INSERT INTO actions (action_id, user_object_id, name, timestamp_opened, timestamp_closed, redo_of) VALUES (?, ?, ?, ?, ?, ?)", (action.id, action.user_object_id, action.name, action.timestamp_opened, action.timestamp_closed, action.redo_of))        
-        Action.conn.commit()
+        action = Action(name = name, closeable = closeable)
+        action.log() # Puts the action in the database.
         return action
+    
+    def log(self):
+        """Log the action to the database."""
+        Action.conn.cursor().execute("INSERT INTO actions (action_id, user_object_id, name, timestamp_opened, timestamp_closed, redo_of) VALUES (?, ?, ?, ?, ?, ?)", (self.id, self.user_object_id, self.name, self.timestamp_opened, self.timestamp_closed, self.redo_of))        
+        Action.conn.commit()
     
     def close(self):
         """Closeable only if called from inside the same method that opened it."""
+        from research_object import ResearchObject
         if not self.closeable:
             return
-        cursor = self.conn.cursor()
-        self.timestamp = datetime.datetime.utcnow()
-        sqlquery = f"UPDATE actions SET timestamp_closed = '{str(self.timestamp)}' WHERE action_id = '{self.id}'"
+        cursor = Action.conn.cursor()
+        self.timestamp_closed = datetime.datetime.utcnow()
+        sqlquery = f"UPDATE actions SET timestamp_closed = '{str(self.timestamp_closed)}' WHERE action_id = '{self.id}'"
         cursor.execute(sqlquery)
+
+        # Set the current action as the one that is being redone.
+        sqlquery = f"INSERT INTO settings (user_object_id, setting_name, setting_value) VALUES ('{self.user_object_id}', {ResearchObject._get_attr_id('current_action_id')}, '{self.id}')"
+        cursor.execute(sqlquery)
+        Action.conn.commit()
 
     @abstractmethod
     def get_open(closeable: bool = True) -> "Action":
@@ -119,7 +175,6 @@ class Action():
         cursor = Action.conn.cursor()
         sqlquery = "SELECT id FROM actions WHERE timestamp IS NULL"
         result = cursor.execute(sqlquery).fetchone()
-        result = []
         if len(result) == 0:
             return None
         id = result[0]
