1aad343d5feb32dd2c0ac30a01e9d498ee871b87
Mitchell Tillman <mtillman14@gmail.com>
2024-01-05 19:50:47 -0600

    Need to be able to run my pipeline.

--- src/ResearchOS/examples/my_pipeline.py
+++ src/ResearchOS/examples/my_pipeline.py
@@ -1,14 +1,24 @@
 """A Python port of my data analysis pipeline."""
 
-import ResearchOS as ros
+from src.ResearchOS.PipelineObjects.project import Project
+from src.ResearchOS.PipelineObjects.analysis import Analysis
+from src.ResearchOS.PipelineObjects.logsheet import Logsheet
+from src.ResearchOS.DataObjects.dataset import Dataset
+from src.ResearchOS.DataObjects.subject import Subject
+from src.ResearchOS.DataObjects.visit import Visit
+from src.ResearchOS.DataObjects.trial import Trial
+from src.ResearchOS.DataObjects.phase import Phase
+from src.ResearchOS.PipelineObjects.subset import Subset
+from src.ResearchOS.PipelineObjects.process import Process
+from src.ResearchOS.variable import Variable
 
-pj= ros.Project(id = "PJE55E00_0CE")  # Create a new project and analysis
-an = ros.Analysis(id = "AN1B61E9_5D4")  # Create a new analysis
+pj= Project(id = "PJE55E00_0CE")  # Create a new project and analysis
+an = Analysis(id = "AN1B61E9_5D4")  # Create a new analysis
 pj.current_analysis_id = an.id  # Set the current analysis for the project
-lg = ros.Logsheet(id = "LGB4002C_900")  # Create a new logsheet
+lg = Logsheet(id = "LGB4002C_900")  # Create a new logsheet
 an.current_logsheet_id = lg.id  # Set the current logsheet for the analysis
 
-ds = ros.Dataset(id = "DS4E22F9_437")  # Create a new dataset
+ds = Dataset(id = "DS4E22F9_437")  # Create a new dataset
 pj.current_dataset_id = ds.id  # Set the current dataset for the project
 
 pj.project_path = "C:/Users/Owner/Documents/ResearchOS"  # Set the project path
@@ -19,7 +29,7 @@ lg.logsheet_headers = []  # Set the logsheet headers
 lg.class_column_names = {}  # Set the class column names
 lg.num_header_rows = 3  # Set the number of header rows
 
-ds.schema = [ros.Dataset, ros.Subject, ros.Visit, ros.Trial, ros.Phase]  # Set the dataset schema
+ds.schema = [Dataset, Subject, Visit, Trial, Phase]  # Set the dataset schema
 
 vrs = lg.read_logsheet()  # Read the logsheet
 print(vrs)  # Print the variables so I know which ones were created.
@@ -27,22 +37,22 @@ print(vrs)  # Print the variables so I know which ones were created.
 def method():
     pass
 
-ss_slg = ros.Subset(id = "SS6BC411_7E1")
-ss_slg.level = ros.Trial
-# ss_slg.conditions["and"] = {}
+ss_slg = Subset(id = "SS6BC411_7E1")
+ss_slg.level = Trial
 ss_slg.conditions["and"]["condition1"] = ["VR5C539C_608", "==", "test"]
 ss_slg.conditions["and"]["condition2"] = ["VR5C539C_608", "==", "test"]
 ss_slg.conditions["and"]["condition3"] = ["VR5C539C_608", "==", "test"]
 
-pr_import = ros.Process(id = "PR640E80_AF2")
-pr_import.level = ros.Process
+pr_import = Process(id = "PR640E80_AF2")
+pr_import.level = Process
 pr_import.method = method
 pr_import.add_subset_id(ss_slg.id)
-vr_fps_used = ros.Variable(id = "VR5C539C_608")
+vr_fps_used = Variable(id = "VR5C539C_608")
 vr_fps_used.hard_coded_value = "test"
 vr_abs_fps_used = vr_fps_used.get_abstract_object()
 vr_abs_fps_used.is_hard_coded = True
 pr_import.add_input_variable_id(id = vr_fps_used.id, name_in_code = "fps_used")
 
-vr_mocap_data = ros.Variable(id = "VR613E29_FFA")
+vr_mocap_data = Variable(id = "VR613E29_FFA")
 pr_import.add_output_variable_id(id = vr_mocap_data.id, name_in_code = "mocap_data")
+pr_import.run_method()
