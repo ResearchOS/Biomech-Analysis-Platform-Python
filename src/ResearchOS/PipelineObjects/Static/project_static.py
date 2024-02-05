a62717de1cc17ede9e0d8d9210a5c6167a75c0e4
Mitchell Tillman <mtillman14@gmail.com>
2024-02-05 18:23:06 -0500

    Renamed Static classes to exclude the "static" suffix.

--- src/ResearchOS/PipelineObjects/Static/project_static.py
+++ src/ResearchOS/PipelineObjects/Static/project_static.py
@@ -1,5 +1,5 @@
 from ResearchOS import PipelineObject
 
-class ProjectStatic(PipelineObject):
+class Project(PipelineObject):
     """The static attributes and methods for the Project class.
     Am I sure that this one is needed?"""
